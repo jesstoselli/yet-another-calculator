aa41fb3e01cde58e235aa4a7d4cbe924844eccb1
jesstoselli <jfamedeiros@gmail.com>
2020-09-18 19:42:24 -0300

    trying to solve deploy error

--- src/pages/Calculator/index.tsx
+++ src/pages/Calculator/index.tsx
@@ -1,9 +1,73 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 import Button from "../../components/Button";
 
 import { Container, Screen, Pad } from "./styles";
 
 const Calculator: React.FC = () => {
+  const [currentValue, setCurrentValue] = useState(0);
+  const [currentOperator, setCurrentOperator] = useState(0);
+
+  const [operations, setOperations] = useState([]);
+
+  const handleClick = useCallback((btn: string) => {
+    let operator = "";
+    let value: string = "";
+    if (
+      btn === "÷" ||
+      btn === "x" ||
+      btn === "-" ||
+      btn === "+" ||
+      btn === "±" ||
+      btn === "C"
+    ) {
+      operator = btn;
+    } else {
+      value = btn;
+    }
+
+    console.log(btn);
+    console.log(value);
+    console.log(operator);
+
+    // let valueToUse: string[] = [];
+    // switch (btn) {
+    //   case value === btn:
+    //     valueToUse.push(value);
+    //     console.log(valueToUse);
+    //     break;
+    //   case value === "÷":
+    //     let finalValue = Number(valueToUse.join(""));
+    //     let previousValue = currentValue;
+    //     setCurrentValue(previousValue / finalValue);
+    //     break;
+    //   case value === "x":
+    //     finalValue = Number(valueToUse.join(""));
+    //     previousValue = currentValue;
+    //     setCurrentValue(previousValue * finalValue);
+    //     break;
+    //   case value === "-":
+    //     finalValue = Number(valueToUse.join(""));
+    //     previousValue = currentValue;
+    //     setCurrentValue(previousValue - finalValue);
+    //     break;
+    //   case value === "+":
+    //     finalValue = Number(valueToUse.join(""));
+    //     previousValue = currentValue;
+    //     setCurrentValue(previousValue + finalValue);
+    //     break;
+    //   case value === "±":
+    //     finalValue = Number(valueToUse.join("")) * -1;
+    //     setCurrentValue(finalValue);
+    //     break;
+    //   case value === "C":
+    //     setCurrentValue(0);
+    //     break;
+
+    //   default:
+    //     break;
+    // }
+  }, []);
+
   const buttonContent = [
     "÷",
     "x",
@@ -27,12 +91,12 @@ const Calculator: React.FC = () => {
       <Screen>
         <p>10 + 12</p>
         <hr />
-        <h4>22</h4>
+        <h4>{currentValue}</h4>
       </Screen>
 
       <Pad>
         {buttonContent.map((btn) => (
-          <Button key={btn} digit={btn} />
+          <Button key={btn} digit={btn} onClick={() => handleClick(btn)} />
         ))}
         <Button key='=' digit='=' className='enter' />
       </Pad>
